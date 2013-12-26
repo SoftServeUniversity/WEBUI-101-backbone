8c0a3ec88fd9a073e4659a6d9eb2d825c9dadee6
rpelyush <rpelyush@gmail.com>
2013-12-26 16:14:22 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketApp::Application.config.secret_key_base = '054655b938288f39944d96d0c49b665ff944d0c1d140f9274536278a94e9bed0d8eb03c217f48a0a85179d3676bda87dd836e6e353896979b9f7f9098c641357'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MarketApp::Application.config.session_store :cookie_store, key: '_market_app_session'
